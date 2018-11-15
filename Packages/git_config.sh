c950f45c01aca7f83501d787b64d3edbdb67e29e
Bogi Thomsen <bogi1511@gmail.com>
2018-11-15 12:37:38 +0100

    begin scripts

---
+++ Packages/git_config.sh
@@ -0,0 +1,10 @@
+## Set vars ##
+EMAIL="email@gmail.com"
+AUTHOR="Bogi Thomsen"
+
+## Set name and email ##
+git config --global user.name $AUTHOR
+git config --global user.email $NAME
+
+## Generate SSH key ##
+ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/id_rsa -N ''
