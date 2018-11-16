43016191bbc8535a8640957f8b0371cb853f5667
Bogi Thomsen <bogi1511@gmail.com>
2018-11-16 09:00:26 +0100

    Update and rename git_config.sh to aur_install.sh

---
+++ Packages/aur_install.sh
@@ -0,0 +1,22 @@
+echo "------------------------------------"
+echo "------ Updating AUR packages -------"
+echo "------------------------------------"
+yaourt -Syu --noconfirm
+## Adding discord gpg key
+gpg --recv-keys 0FC3042E345AD05D
+
+echo "------------------------------------"
+echo "----- Installing AUR packages ------"
+echo "------------------------------------"
+yaourt -S --noconfirm --needed \
+  spotify sublime-text-dev slack-desktop discord
+  
+if [ $? != 0 ]; then
+  echo ""
+  echo "ERROR running yaourt -- must check if all packages are available"
+  exit 1
+fi
+
+echo "------------------------------------"
+echo "-------- Install finished ----------"
+echo "------------------------------------"
