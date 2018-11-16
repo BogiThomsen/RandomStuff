b815672729090281021efecbcb59b53d239e5b73
Bogi Thomsen <bogi1511@gmail.com>
2018-11-16 11:39:50 +0100

    Update post_install.sh

--- Packages/post_install.sh
+++ Packages/post_install.sh
@@ -35,14 +35,19 @@ echo "------------------------------------"
 echo "--------- Configuring git ----------"
 echo "------------------------------------"
 ## Set vars
-EMAIL="email@gmail.com"
-AUTHOR="Bogi Thomsen"
+echo "Enter email address:"
+read EMAIL
+echo "Enter Username:"
+read USERNAME
 
 ## Set name and emails
-git config --global user.name $AUTHOR
+git config --global user.name $USERNAME
 git config --global user.email $EMAIL
 
 ssh-keygen -t rsa -b 4096 -C "$EMAIL"
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
 
 echo "------------------------------------"
 echo "-------- Install finished ----------"
