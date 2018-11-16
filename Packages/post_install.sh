737ec666f873e92b1f6c3ddca7f2fc6ac9112f0b
Bogi Thomsen <bogi1511@gmail.com>
2018-11-16 12:07:22 +0100

    Update post_install.sh

--- Packages/post_install.sh
+++ Packages/post_install.sh
@@ -30,24 +30,6 @@ if [ $? != 0 ]; then
     echo "ERROR running pacman -- must check if all packages are available"
     exit 1
 fi
-               
-echo "------------------------------------"
-echo "--------- Configuring git ----------"
-echo "------------------------------------"
-## Set vars
-echo "Enter email address:"
-read EMAIL
-echo "Enter Username:"
-read USERNAME
-
-## Set name and emails
-git config --global user.name $USERNAME
-git config --global user.email $EMAIL
-
-ssh-keygen -t rsa -b 4096 -C "$EMAIL"
-
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
 
 echo "------------------------------------"
 echo "-------- Install finished ----------"
